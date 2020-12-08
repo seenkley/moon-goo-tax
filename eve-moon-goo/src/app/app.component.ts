691b971cba180282c4c677ceccea80ce2b833063
Juergen <juergen.stuermer@liwest.at>
2020-12-08 18:29:11 +0100

    add documentation

--- eve-moon-goo/src/app/app.component.ts
+++ eve-moon-goo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalServiceService } from './services/modal-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Valkyrie Moon Tax System';
+
+  constructor(private modalService: ModalServiceService) {
+    
+  }
+
+  openModal(content) {
+    this.modalService.openModalLg(content);
+  }
+
 }
