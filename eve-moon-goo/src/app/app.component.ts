e84464b4d6038d2d2988f8070bee240c2aa497f9
Juergen <juergen.stuermer@liwest.at>
2021-05-15 11:02:26 +0200

    fetch holding corp dynamically

--- eve-moon-goo/src/app/app.component.ts
+++ eve-moon-goo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FetchServiceService } from './services/fetch-service.service';
 import { ModalServiceService } from './services/modal-service.service';
 
 @Component({
@@ -8,9 +10,14 @@ import { ModalServiceService } from './services/modal-service.service';
 })
 export class AppComponent {
   title = 'Valkyrie Moon Tax System';
+  holdingCorp$: Observable<string>;
 
-  constructor(private modalService: ModalServiceService) {
+  constructor(private modalService: ModalServiceService, private fetchService: FetchServiceService) {
     
+  }
+
+  ngOnInit() {
+    this.holdingCorp$ = this.fetchService.getHoldingCorp();
   }
 
   openModal(content) {
